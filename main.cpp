6a197b32c1c035335942404fb329c923373e307e
Soham Kukreti <kukretisoham@gmail.com>
2023-11-17 21:37:09 +0530

    Added custom start and dest grid box and improved obstacles

--- main.cpp
+++ main.cpp
@@ -20,12 +20,12 @@ int main()
     blackRectangle.setFillColor(Color::Black);
     RectangleShape redRectangle(Vector2f(10,10));   //destination grid box
     redRectangle.setFillColor(Color::Red);
-    redRectangle.setOutlineThickness(2);
+    //redRectangle.setOutlineThickness(2);
     redRectangle.setOutlineColor(Color::Blue);
-    RectangleShape blueRectangle(Vector2f(10,10)); // source grid box
-    blueRectangle.setFillColor(Color::Green);
-    blueRectangle.setOutlineThickness(2);
-    blueRectangle.setOutlineColor(Color::Black);
+    RectangleShape GreenRectangle(Vector2f(10,10)); // source grid box
+    GreenRectangle.setFillColor(Color::Green);
+    //blueRectangle.setOutlineThickness(2);
+    GreenRectangle.setOutlineColor(Color::Black);
 
     int source_x=2,source_y=2,dest_x=50,dest_y=56;      //Origin(2,3)->Goal(50,56)
 
@@ -37,12 +37,14 @@ int main()
         }
     }
 
+    /*
     for(int i = 0 ; i < 60;i++){
         for(int j = 0;j<60;j++){
             cout << grid[i][j] << " ";
         }
         cout << endl;
     }
+    */
 
     while(window.isOpen()){
         sf::Event evnt;
@@ -53,18 +55,45 @@ int main()
             if(evnt.type == evnt.Closed){
                 window.close();
             } 
-            if(evnt.type==Event::MouseButtonPressed && evnt.mouseButton.button==Mouse::Left){
-                int x = evnt.mouseButton.x;
-                int y = evnt.mouseButton.y;
+            if(Mouse::isButtonPressed(Mouse::Left)){
+                int x = Mouse::getPosition(window).x;
+                int y = Mouse::getPosition(window).y;
+                int row=y/10;
+                int col = x/10;
+                if(grid[row][col] == 1 && (row < 60 && col < 60)){
+                    grid[row][col] = 0;
+                }
+            }
+            if(Mouse::isButtonPressed(Mouse::Right)){
+                int x = Mouse::getPosition(window).x;
+                int y = Mouse::getPosition(window).y;
                 int row=y/10;
                 int col = x/10;
                 if(grid[row][col] == 0 && (row < 60 && col < 60)){
                     grid[row][col] = 1;
                 }
-                else if(grid[row][col] == 1 && (row < 60 && col < 60)){
-                    grid[row][col] = 0;
-                }
-            }  
+
+            }
+            //adding source grid box
+            if(evnt.type == Event::KeyPressed && evnt.key.code == Keyboard::S){
+                int mouse_x = Mouse::getPosition(window).x;
+                int mouse_y = Mouse::getPosition(window).y;
+                cout << mouse_x << " " <<mouse_y << endl;
+                source_x=mouse_y/10;
+                source_y = mouse_x/10;
+                cout << "You tried to enter source grid box.";
+                
+            }
+            //adding destination grid box
+            if(evnt.type == Event::KeyPressed && evnt.key.code == Keyboard::D){
+               int mouse_x = Mouse::getPosition(window).x;
+                int mouse_y = Mouse::getPosition(window).y;
+                cout << mouse_x << " " <<mouse_y << endl;
+                dest_x=mouse_y/10;
+                dest_y = mouse_x/10;
+                cout << "You tried to enter source grid box.";
+            }
+
         }
 
         
@@ -73,15 +102,15 @@ int main()
             for(int j = 0;j<=590;j+=10){
                 //adding the default white cells
                 if(grid[i/10][j/10] == 1){
-                    rectangle.setOutlineThickness(2);
-                    rectangle.setOutlineColor(Color::Blue);
+                    rectangle.setOutlineThickness(1);
+                    rectangle.setOutlineColor(Color::Black);
                     rectangle.setPosition(j,i);
                     window.draw(rectangle);   
 
                 }
                 //adding black border cells
                 else if(grid[i/10][j/10] == 0){
-                    blackRectangle.setOutlineThickness(2);
+                    blackRectangle.setOutlineThickness(1);
                     blackRectangle.setOutlineColor(Color::Red);
                     blackRectangle.setPosition(j,i);
                     window.draw(blackRectangle);
@@ -90,8 +119,8 @@ int main()
             }
         }
 
-        blueRectangle.setPosition(source_y*10,source_x*10);
-        window.draw(blueRectangle);     //source
+        GreenRectangle.setPosition(source_y*10,source_x*10);
+        window.draw(GreenRectangle);     //source
         filled[source_x][source_y]=1;
         redRectangle.setPosition(dest_y*10,dest_x*10);
         window.draw(redRectangle);        //destination
